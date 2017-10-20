9460c8c4c9a354950378115a46fedffe6795d7e5
George Fraser <george@fivetran.com>
2017-10-19 21:37:00 -0700

    Runnable example

--- src/Main/Program.fs
+++ src/Main/Program.fs
@@ -1,8 +1,20 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open Microsoft.FSharp.Compiler.SourceCodeServices
+
+let file = "MyScript.fsx"
+let input = """
+let foo () = "foo!""
+"""
+let checker = FSharpChecker.Create()
+let projOptions, projOptionsErrors = checker.GetProjectOptionsFromScript(file, input) |> Async.RunSynchronously
+let parsingOptions, parsingOptionsErrors = checker.GetParsingOptionsFromProjectOptions(projOptions)
+let parseFileResults = checker.ParseFile(file, input, parsingOptions) |> Async.RunSynchronously
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    for error in parseFileResults.Errors do 
+        printfn "%d:%d %s" error.StartLineAlternate error.StartColumn error.Message
+    printfn "Finished %s" parseFileResults.FileName
     0 // return an integer exit code
