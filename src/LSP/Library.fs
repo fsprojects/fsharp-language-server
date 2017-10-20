3042d7a9805a5c54881881194805fa8b55e9355c
George Fraser <george@fivetran.com>
2017-10-20 10:10:22 -0700

    Parse headers

--- src/LSP/Library.fs
+++ src/LSP/Library.fs
@@ -1,5 +1,29 @@
 namespace LSP
 
-module Say =
-    let hello name =
-        sprintf "Hello %s" name
+open System
+open System.Net.Sockets
+
+type Request = TodoRequest
+type Response = TodoResponse
+type ILanguageServer = 
+    abstract member Start: requests: seq<Request> -> seq<Response>
+
+module Parser = 
+    type Header = ContentLength of int | Other
+
+    let parseHeader (header: string): Header = 
+        let contentLength = "Content-Length: "
+        if header.StartsWith contentLength then
+            let tail = header.Substring (contentLength.Length)
+            let length = Int32.Parse tail 
+            ContentLength(length)
+        else Other
+
+    let messages (client: Socket): seq<string> = 
+        raise (NotImplementedException())
+
+    let parse (body: string): Request = 
+        raise (NotImplementedException())
+
+    let stringify (response: Response): string = 
+        raise (NotImplementedException())
--- tests/LSP.Tests/Tests.fs
+++ tests/LSP.Tests/Tests.fs
@@ -1,8 +1,16 @@
-module LSP.Tests
+namespace LSP
 
 open NUnit.Framework
 
-[<Test>]
-let ``say hello`` () = 
-    let helloGeorge = Say.hello "George"
-    Assert.That(helloGeorge, Is.EqualTo("Hello George"))
+module ParserTests =
+    [<Test>]
+    let ``parse content length header`` () = 
+        let sample = "Content-Length: 10"
+        let parsed = Parser.parseHeader sample 
+        Assert.That(parsed, Is.EqualTo(Parser.ContentLength(10)))
+
+    [<Test>]
+    let ``parse content type header`` () = 
+        let sample = "Content-Type: application/vscode-jsonrpc; charset=utf-8"
+        let parsed = Parser.parseHeader sample 
+        Assert.That(parsed, Is.EqualTo(Parser.Other))
