d147786f6117cab38d019090b30f509f45022aeb
George Fraser <george@fivetran.com>
2017-10-21 22:06:16 -0700

    Parse outer wrapper

--- src/LSP/Parser.fs
+++ src/LSP/Parser.fs
@@ -2,9 +2,16 @@ namespace LSP
 
 open System
 open FSharp.Data
+open FSharp.Data.JsonExtensions
 
 module Parser = 
     type RequestMessage = RequestMessage of id: int * method: string * params: option<JsonValue>
 
-    let parseRequestMessage (json: string): RequestMessage = 
-        raise (NotImplementedException())
+    let parseRequestMessage (jsonText: string): RequestMessage = 
+        let json = JsonValue.Parse jsonText
+        let jsonRpcVersion = json.GetProperty("jsonrpc").AsString()
+        assert (jsonRpcVersion = "2.0")
+        let id = json.GetProperty("id").AsInteger()
+        let method = json.GetProperty("method").AsString()
+        let params = json.TryGetProperty("params")
+        RequestMessage (id, method, params)
