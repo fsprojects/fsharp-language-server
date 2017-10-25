8624382ae04d5a5c23d44e4b733d79d1f8ac1d34
George Fraser <george@fivetran.com>
2017-10-24 18:35:23 -0700

    Test string-equals ignoring space

--- TODO.md
+++ TODO.md
@@ -3,6 +3,7 @@
 - ``some-name`` is highlighted wrong at -
 - ``name`` is highlighted like a function name regardless of context
 - "string format %d" doesn't format %d
+- @"\w" doesn't highlight regexes
 - Missing keywords:
   - to
   - downto
--- tests/LSP.Tests/LanguageServerTests.fs
+++ tests/LSP.Tests/LanguageServerTests.fs
@@ -5,6 +5,7 @@ open Parser
 open LanguageServer
 open System.Runtime.Serialization
 open NUnit.Framework
+open System.Text.RegularExpressions
 
 [<DataContract>]
 type Simple = {
@@ -16,3 +17,16 @@ type Simple = {
 let ``serialize a simple record class to JSON`` () = 
     let serializer = serializerFactory<Simple>()
     Assert.That(serializer({simpleProp = 1}), Is.EqualTo("""{"simpleProp":1}"""))
+
+let removeSpace (expected: string) = 
+    Regex.Replace(expected, @"\s", "")
+
+[<Test>]
+let ``remove space from string`` () = 
+    Assert.That(removeSpace "foo bar", Is.EqualTo "foobar")
+
+[<Test>]
+let ``remove newline from string`` () = 
+    let actual = """foo 
+    bar"""
+    Assert.That(removeSpace actual, Is.EqualTo "foobar")
