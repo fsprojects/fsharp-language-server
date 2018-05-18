f385dbc3f3d1ec496f3f26259a3b828cbd8568cd
George Fraser <george@fivetran.com>
2018-05-18 10:12:50 -0700

    Formatting

--- tests/LSP.Tests/LanguageServerTests.fs
+++ tests/LSP.Tests/LanguageServerTests.fs
@@ -16,19 +16,19 @@ let binaryWriter() =
     let writer = new BinaryWriter(stream)
     let toString() = 
         let bytes = stream.ToArray() 
-        Encoding.UTF8.GetString bytes
-    (writer, toString)
+        Encoding.UTF8.GetString(bytes)
+    writer, toString
 
 [<Test>]
-let ``write text`` () = 
-    let (writer, toString) = binaryWriter() 
-    writer.Write (Encoding.UTF8.GetBytes "foo")
+let ``write text``() = 
+    let writer, toString = binaryWriter() 
+    writer.Write(Encoding.UTF8.GetBytes "foo")
     let found = toString()
     Assert.AreEqual("foo", found)
 
 [<Test>]
-let ``write response`` () = 
-    let (writer, toString) = binaryWriter() 
+let ``write response``() = 
+    let writer, toString = binaryWriter() 
     LanguageServer.respond(writer, 1, "2")
     let expected = "Content-Length: 19\r\n\r\n\
                     {\"id\":1,\"result\":2}"
@@ -36,8 +36,8 @@ let ``write response`` () =
     Assert.AreEqual(expected, found)
 
 [<Test>]
-let ``write multibyte characters`` () = 
-    let (writer, toString) = binaryWriter() 
+let ``write multibyte characters``() = 
+    let writer, toString = binaryWriter() 
     LanguageServer.respond(writer, 1, "🔥")
     let expected = "Content-Length: 22\r\n\r\n\
                     {\"id\":1,\"result\":🔥}"
@@ -87,9 +87,9 @@ let messageStream(messages: string list): BinaryReader =
     let stdin = new MemoryStream()
     for m in messages do 
         let trim = m.Trim()
-        let length = Encoding.UTF8.GetByteCount trim
+        let length = Encoding.UTF8.GetByteCount(trim)
         let wrapper = sprintf "Content-Length: %d\r\n\r\n%s" length trim
-        let bytes = Encoding.UTF8.GetBytes wrapper 
+        let bytes = Encoding.UTF8.GetBytes(wrapper) 
         stdin.Write(bytes, 0, bytes.Length)
     stdin.Seek(int64 0, SeekOrigin.Begin) |> ignore
     new BinaryReader(stdin)
@@ -104,11 +104,11 @@ let initializeMessage = """
 """
 
 [<Test>]
-let ``read messages from a stream`` () = 
+let ``read messages from a stream``() = 
     let stdin = messageStream [initializeMessage]
-    let messages = LanguageServer.readMessages stdin
-    let found = Seq.toList messages
-    Assert.AreEqual([Parser.RequestMessage (1, "initialize", JsonValue.Parse "{}")], found)
+    let messages = LanguageServer.readMessages(stdin)
+    let found = Seq.toList(messages)
+    Assert.AreEqual([Parser.RequestMessage(1, "initialize", JsonValue.Parse "{}")], found)
 
 let exitMessage = """
 {
@@ -118,29 +118,29 @@ let exitMessage = """
 """
     
 [<Test>]
-let ``exit message terminates stream`` () = 
+let ``exit message terminates stream``() = 
     let stdin = messageStream [initializeMessage; exitMessage; initializeMessage]
-    let messages = LanguageServer.readMessages stdin
+    let messages = LanguageServer.readMessages(stdin)
     let found = Seq.toList messages
-    Assert.AreEqual([Parser.RequestMessage (1, "initialize", JsonValue.Parse "{}")], found)
+    Assert.AreEqual([Parser.RequestMessage(1, "initialize", JsonValue.Parse "{}")], found)
     
 [<Test>]
-let ``end of bytes terminates stream`` () = 
+let ``end of bytes terminates stream``() = 
     let stdin = messageStream [initializeMessage]
-    let messages = LanguageServer.readMessages stdin
+    let messages = LanguageServer.readMessages(stdin)
     let found = Seq.toList messages
-    Assert.AreEqual([Parser.RequestMessage (1, "initialize", JsonValue.Parse "{}")], found)
+    Assert.AreEqual([Parser.RequestMessage(1, "initialize", JsonValue.Parse "{}")], found)
 
 let mock(server: ILanguageServer) (messages: string list): string = 
     let stdout = new MemoryStream()
     let writeOut = new BinaryWriter(stdout)
-    let readIn = messageStream messages
+    let readIn = messageStream(messages)
     let serverFactory = fun _ -> server
     LanguageServer.connect(serverFactory, readIn, writeOut)
     Encoding.UTF8.GetString(stdout.ToArray())
 
 [<Test>]
-let ``send Initialize`` () = 
+let ``send Initialize``() = 
     let message = """
     {
         "jsonrpc": "2.0",
