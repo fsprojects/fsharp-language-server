c885bcae7f2e787665943a35eac80cdb8b626575
George Fraser <george@fivetran.com>
2018-06-03 09:43:40 -0700

    Comment

--- src/LSP/Log.fs
+++ src/LSP/Log.fs
@@ -2,5 +2,6 @@ module LSP.Log
 
 let diagnosticsLog = ref stderr
 
+/// Print to LSP.Log.diagnosticsLog, which is stderr by default but can be redirected
 let dprintfn(fmt: Printf.TextWriterFormat<'T>): 'T = 
     Printf.fprintfn !diagnosticsLog fmt
