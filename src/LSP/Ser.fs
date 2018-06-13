34751b5bad0f69224e30270f4d18f0db32f733b4
tboby <th.boby@gmail.com>
2018-06-13 05:50:48 +0100

    Handle escaped tabs and backslashes (#6)
    * Handle escaped tabs
    
    This deals with "\t".
    
    * Handle slashes

--- src/LSP/Ser.fs
+++ src/LSP/Ser.fs
@@ -7,10 +7,12 @@ open Microsoft.FSharp.Reflection.FSharpReflectionExtensions
 open System.Text.RegularExpressions
 open FSharp.Data
 
-let private escapeChars = Regex("[\n\r\"]", RegexOptions.Compiled)
+let private escapeChars = Regex("[\t\n\r\"\\\\]", RegexOptions.Compiled)
 let private replaceChars = 
     MatchEvaluator(fun m -> 
         match m.Value with 
+        | "\\" -> "\\\\"
+        | "\t" -> "\\t"
         | "\n" -> "\\n" 
         | "\r" -> "\\r" 
         | "\"" -> "\\\"" 
