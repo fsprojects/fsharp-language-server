63a18087efd8e3308010983abc7059ac895c2e4e
Adam Becker <im.adam.becker@gmail.com>
2018-09-13 18:45:07 -0600

    I think this is all I have to do.

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,6 +10,7 @@ npm install
 dotnet clean
 dotnet publish -c Release -r win10-x64 src/FSharpLanguageServer
 dotnet publish -c Release -r osx.10.11-x64 src/FSharpLanguageServer
+dotnet publish -c Release -r linux-x64 src/FSharpLanguageServer
 
 # Build vsix
 vsce package -o build.vsix
