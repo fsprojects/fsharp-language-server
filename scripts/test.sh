19aea6ea38bd1e35ab7341455e429de4bc481f36
George Fraser <george@fivetran.com>
2018-11-02 00:34:10 -0700

    Centralize restore script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,20 +1,7 @@
 #!/usr/bin/env bash
-# Build the test projects and run all tests
+# Restore test projects and run all tests
 
-echo 'Building sample projects...'
-dotnet restore sample/EmptyProject/EmptyProject.fsproj
-dotnet restore sample/MainProject/MainProject.fsproj
-dotnet restore sample/HasLocalDll/HasLocalDll.fsproj
-dotnet restore sample/FSharpKoans.Core/FSharpKoans.Core.fsproj
-dotnet restore sample/HasTests/HasTests.fsproj
-dotnet restore sample/ReferenceCSharp/ReferenceCSharp.fsproj
-dotnet restore sample/ReferenceCSharp.AssemblyName/ReferenceCSharp.AssemblyName.fsproj
-dotnet restore sample/Signature/Signature.fsproj
-dotnet restore sample/HasPackageReference/HasPackageReference.fsproj
-dotnet build sample/CSharpProject/CSharpProject.csproj
-dotnet build sample/CSharpProject.AssemblyName/CSharpProject.AssemblyName.csproj
-dotnet build sample/SlnReferences/ReferencedProject.fsproj
-# Be sure to update .circleci/config.yml when you add sample projects
+source scripts/restore.sh
 
 echo 'Running tests...'
 set -e
