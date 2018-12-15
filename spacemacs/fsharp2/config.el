787997f221ea5fae6073cd808b11867bb437fcf7
Chris Marchetti <cam.marchetti@gmail.com>
2018-12-15 13:07:32 -0500

    Spacemacs layer (#33)
    * Add Spacemacs layer and configuration information
    
    At the moment, Spacemacs fsharp layer does not include a way to
    configure an LSP server, while the bundled fsharp2 does. Including it
    required a addition to the license information since it is derived from
    the GPLv3 fsharp layer.
    
    * Update author information

---
+++ spacemacs/fsharp2/config.el
@@ -0,0 +1,17 @@
+;;; config.el --- fsharp Layer config File for Spacemacs
+;;
+;; Copyright (c) 2012-2018 Sylvain Benner & Contributors
+;;
+;; Author: Chris Marchetti <cam.marchetti@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+(spacemacs|define-jump-handlers fsharp-mode)
+
+(defvar fsharp2-backend 'lsp
+ "The backend to use for IDE features. Possible values are `fsac' and `lsp'")
+
+(defvar fsharp2-lsp-executable "FSharpLanguageServer"
+ "The locatio nof the FSharpLanguageServer executable")
