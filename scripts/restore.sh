89ee5d0a299f2f746fad2759f2ea79b233fe0d82
George Fraser <george@fivetran.com>
2019-04-28 19:39:28 -0700

    Just restore

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 # Restore test projects
 
-echo 'Building sample projects...'
+echo 'Restoring sample projects...'
+dotnet restore sample/CSharpProject.AssemblyName/CSharpProject.AssemblyName.csproj
+dotnet restore sample/CSharpProject/CSharpProject.csproj
 dotnet restore sample/EmptyProject/EmptyProject.fsproj
-dotnet restore sample/MainProject/MainProject.fsproj
-dotnet restore sample/HasLocalDll/HasLocalDll.fsproj
 dotnet restore sample/FSharpKoans.Core/FSharpKoans.Core.fsproj
-dotnet restore sample/HasTests/HasTests.fsproj
-dotnet restore sample/ReferenceCSharp/ReferenceCSharp.fsproj
-dotnet restore sample/ReferenceCSharp.AssemblyName/ReferenceCSharp.AssemblyName.fsproj
-dotnet restore sample/Signature/Signature.fsproj
+dotnet restore sample/HasLocalDll/HasLocalDll.fsproj
 dotnet restore sample/HasPackageReference/HasPackageReference.fsproj
+dotnet restore sample/HasTests/HasTests.fsproj
+dotnet restore sample/Issue28/Issue28.fsproj
+dotnet restore sample/MainProject/MainProject.fsproj
+dotnet restore sample/ReferenceCSharp.AssemblyName/ReferenceCSharp.AssemblyName.fsproj
+dotnet restore sample/ReferenceCSharp/ReferenceCSharp.fsproj
+dotnet restore sample/Signature/Signature.fsproj
+dotnet restore sample/SlnReferences/ReferencedProject.fsproj
 dotnet restore sample/TemplateParams/TemplateParams.fsproj
-# TODO could these be restore instead of build?
-dotnet build sample/CSharpProject/CSharpProject.csproj
-dotnet build sample/CSharpProject.AssemblyName/CSharpProject.AssemblyName.csproj
-dotnet build sample/Issue28/Issue28.fsproj
-dotnet build sample/SlnReferences/ReferencedProject.fsproj
