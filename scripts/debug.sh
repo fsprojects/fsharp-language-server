e67fbeaaf1aa1c4733fdd2dd6dd0600f758fc3fe
George Fraser <george@fivetran.com>
2020-03-17 21:07:31 -0700

    Reset compile outputs

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-# Builds src/FSharpLanguageServer/bin/Release/netcoreapp3.0/osx.10.11-x64/publish/FSharpLanguageServer
+# Delete build outputs
+rm -rf src/*/bin src/*/obj tests/*/bin tests/*/obj node_modules
+
+# Build js
+npm install
+npm run-script compile
+
+# Build src/FSharpLanguageServer/bin/Release/netcoreapp3.0/osx.10.11-x64/publish/FSharpLanguageServer
 dotnet publish -c Release -r osx.10.11-x64 src/FSharpLanguageServer
 echo 'Press F5 to debug the new build of F# language server'
