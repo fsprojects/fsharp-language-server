ef8e8e85a5f15804dd94a1fb7576b0a1bbb7181b
Eli Dowling <eli.jambu@yahoo.com>
2022-03-04 09:07:01 +1000

    Added some important pre build steps to the extension build

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,4 +1,7 @@
-#dotnet clean
+npm install
+paket install
+dotnet restore
+dotnet clean
 dotnet publish -c Release src/FSharpLanguageServer
 #dotnet publish -c Release -r osx.10.11-x64 src/FSharpLanguageServer
 #dotnet publish -c Release -r linux-x64 src/FSharpLanguageServer
