8be99e7b4fd9f4b69d7c1cd2937d33bef18ed246
Eli Dowling <eli.jambu@yahoo.com>
2022-05-14 21:46:55 +1000

    added another check to normalization

--- src/LSP/Utils.fs
+++ src/LSP/Utils.fs
@@ -1,12 +1,15 @@
 namespace LSP
 open System
 open System.IO
+open System.Text.RegularExpressions
 type NormedFileInfo=FileInfo
 [<AutoOpen>]
 module Utils=
     let normalizeDriveLetter (path:string)=
-        let a=path.ToCharArray() 
-        a[0]<-a[0] |>Char.ToUpperInvariant
-        new String(a)
-    let FileInfo path =
+        if  Regex("(^\w:)").Match(path).Success then
+            let a=path.ToCharArray() 
+            a[0]<-(a[0] |>Char.ToUpperInvariant)
+            new String(a)
+        else path    
+    let normedFileInfo path =
         path|> normalizeDriveLetter|>NormedFileInfo
